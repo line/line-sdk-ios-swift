be572caf487611588112a2afd3677900b40551b8
onevcat <wei.wang@linecorp.com>
2018-08-07 13:47:48 +0900

    Improve grammar

--- LineSDK/LineSDK/Utils/Delegate.swift
+++ LineSDK/LineSDK/Utils/Delegate.swift
@@ -21,7 +21,7 @@
 
 import Foundation
 
-/// A delegate helper type to "shadow" weak `self`, to prevent creating a retain cycle unexpected.
+/// A delegate helper type to "shadow" weak `self`, to prevent creating an unexpected retain cycle.
 class Delegate<Input, Output> {
     init() {}
     
