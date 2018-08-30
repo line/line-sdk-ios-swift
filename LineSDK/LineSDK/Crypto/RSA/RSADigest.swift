dd19f57762f82237ca9b9dd15ab98e5d9a317239
onevcat <wei.wang@linecorp.com>
2018-08-30 13:45:46 +0900

    Implement RSA  algorithm

--- LineSDK/LineSDK/Crypto/RSA/RSADigest.swift
+++ LineSDK/LineSDK/Crypto/RSA/RSADigest.swift
@@ -24,7 +24,61 @@ import CommonCrypto
 
 /// RSA Digest Algorithms.
 extension RSA {
+    
+    typealias Digest = (
+        _ data: UnsafeRawPointer?,
+        _ length: CC_LONG,
+        _ md: UnsafeMutablePointer<UInt8>?) -> UnsafeMutablePointer<UInt8>?
+    
     enum Algorithm {
-        case sha1, sha224, sha256, sha384, sha612
+        case sha1, sha224, sha256, sha384, sha512
+        
+        var length: CC_LONG {
+            switch self {
+            case .sha1: return CC_LONG(CC_SHA1_DIGEST_LENGTH)
+            case .sha224: return CC_LONG(CC_SHA224_DIGEST_LENGTH)
+            case .sha256: return CC_LONG(CC_SHA256_DIGEST_LENGTH)
+            case .sha384: return CC_LONG(CC_SHA384_DIGEST_LENGTH)
+            case .sha512: return CC_LONG(CC_SHA512_DIGEST_LENGTH)
+            }
+        }
+        
+        var signatureAlgorithm: SecKeyAlgorithm {
+            switch self {
+            case .sha1:   return .rsaSignatureMessagePKCS1v15SHA1
+            case .sha224: return .rsaSignatureMessagePKCS1v15SHA224
+            case .sha256: return .rsaSignatureMessagePKCS1v15SHA256
+            case .sha384: return .rsaSignatureMessagePKCS1v15SHA384
+            case .sha512: return .rsaSignatureMessagePKCS1v15SHA512
+            }
+        }
+        
+        var encryptionAlgorithm: SecKeyAlgorithm {
+            switch self {
+            case .sha1:   return .rsaEncryptionOAEPSHA1AESGCM
+            case .sha224: return .rsaEncryptionOAEPSHA224AESGCM
+            case .sha256: return .rsaEncryptionOAEPSHA256AESGCM
+            case .sha384: return .rsaEncryptionOAEPSHA384AESGCM
+            case .sha512: return .rsaEncryptionOAEPSHA512AESGCM
+            }
+        }
+        
+        var digest: Digest {
+            switch self {
+            case .sha1:   return CC_SHA1
+            case .sha224: return CC_SHA224
+            case .sha256: return CC_SHA256
+            case .sha384: return CC_SHA384
+            case .sha512: return CC_SHA512
+            }
+        }
+    }
+}
+
+extension Data {
+    func digest(using algorithm: RSA.Algorithm) throws -> Data {
+        var hash = [UInt8](repeating: 0, count: Int(algorithm.length))
+        withUnsafeBytes { _ = algorithm.digest($0, CC_LONG(count), &hash) }
+        return Data(bytes: hash)
     }
 }
