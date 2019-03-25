85d487a7c69bcfccd2e84bc3d35d5756b96fa650
onevcat <wei.wang@linecorp.com>
2019-03-25 14:20:42 +0900

    Move view layout code to better place

--- LineSDK/LineSDK/SharingUI/PageViewController.swift
+++ LineSDK/LineSDK/SharingUI/PageViewController.swift
@@ -31,13 +31,29 @@ class PageViewController: UIViewController {
     private lazy var pageContainerView: UIView = {
         let pageContainerView = UIView()
         view.addSubview(pageContainerView)
+        pageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pageContainerView.topAnchor     .constraint(equalTo: pageTabView.bottomAnchor),
+            pageContainerView.leadingAnchor .constraint(equalTo: view.leadingAnchor),
+            pageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageContainerView.bottomAnchor  .constraint(equalTo: view.bottomAnchor)
+            ])
+
         return pageContainerView
     }()
 
     private lazy var pageTabView: PageTabView = {
         let pageTabView = PageTabView(titles: pages.map { $0.title })
         view.addSubview(pageTabView)
+        pageTabView.translatesAutoresizingMaskIntoConstraints = false
         pageTabView.delegate = self
+        NSLayoutConstraint.activate([
+            pageTabView.heightAnchor  .constraint(equalToConstant: 45),
+            pageTabView.leadingAnchor .constraint(equalTo: view.leadingAnchor),
+            pageTabView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageTabView.topAnchor     .constraint(equalTo: safeTopAnchor)
+            ])
+
         return pageTabView
     }()
 
@@ -66,33 +82,10 @@ class PageViewController: UIViewController {
         super.viewDidLoad()
 
         setupPageViewController()
+        setupSubviews()
 
         view.backgroundColor = .white
         pageTabView.backgroundColor = .yellow
-
-        view.addSubview(pageTabView)
-        view.addSubview(pageContainerView)
-
-        pageTabView.translatesAutoresizingMaskIntoConstraints = false
-        pageContainerView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            pageTabView.heightAnchor  .constraint(equalToConstant: 45),
-            pageTabView.leadingAnchor .constraint(equalTo: view.leadingAnchor),
-            pageTabView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            pageTabView.topAnchor     .constraint(equalTo: safeTopAnchor)
-            ])
-
-        NSLayoutConstraint.activate([
-            pageContainerView.topAnchor     .constraint(equalTo: pageTabView.bottomAnchor),
-            pageContainerView.leadingAnchor .constraint(equalTo: view.leadingAnchor),
-            pageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            pageContainerView.bottomAnchor  .constraint(equalTo: view.bottomAnchor)
-            ])
-
-        view.layoutIfNeeded()
-        pageTabView.resetUnderline()
-
 
         pageScrollViewObserver = pageScrollView?.observe(\.contentOffset, options: [.new]) { [weak self] scrollView, change in
             guard let self = self else { return }
@@ -103,6 +96,13 @@ class PageViewController: UIViewController {
         }
 
         pageScrollView?.delegate = self
+    }
+
+    private func setupSubviews() {
+        _ = pageContainerView
+        _ = pageTabView
+        view.layoutIfNeeded()
+        pageTabView.resetUnderline()
     }
 
     private func setupPageViewController() {
