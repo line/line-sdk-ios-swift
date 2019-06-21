a2df33e6d6d67468cacedc289af9b416acf8e895
wouter-veeken <mail.wouter@gmail.com>
2019-06-21 16:20:14 +0900

    Fix another unmatched quote error

--- LineSDK/LineSDK/SharingUI/Public/ShareViewControllerDelegate.swift
+++ LineSDK/LineSDK/SharingUI/Public/ShareViewControllerDelegate.swift
@@ -196,7 +196,7 @@ extension ShareViewControllerDelegate {
             Log.fatalError(
                 """
                 You need at least set the `ShareViewController.message` or implement
-                `shareViewController(:messageForSendingToTargets:)` before sharing a message.")
+                `shareViewController(:messageForSendingToTargets:)` before sharing a message.)
                 """
             )
         }
