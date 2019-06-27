89eaba870eb2f35566136f3297da85c59d15fd25
onevcat <wei.wang@linecorp.com>
2019-06-27 16:28:11 +0900

    Fix exclude path for UI

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .target(
             name: "LineSDK",
             path: "LineSDK/LineSDK",
-            exclude: ["LineSDK/LineSDK/LineSDKUI"]
+            exclude: ["LineSDKUI"]
         )
     ]
 )
