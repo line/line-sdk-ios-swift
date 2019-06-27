60a1afa0ee97f94175b7f3d0f1bee8e817c8e6a4
onevcat <wei.wang@linecorp.com>
2019-06-27 17:38:12 +0900

    Add new target for SPM testing

---
+++ LineSDKSample/LineSDKSample/UI/SampleUIHomeViewController+SPM.swift
@@ -0,0 +1,36 @@
+//
+//  SampleUIHomeViewController.swift
+//
+//  Copyright (c) 2016-present, LINE Corporation. All rights reserved.
+//
+//  You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+//  copy and distribute this software in source code or binary form for use
+//  in connection with the web services and APIs provided by LINE Corporation.
+//
+//  As with any software that integrates with the LINE Corporation platform, your use of this software
+//  is subject to the LINE Developers Agreement [http://terms2.line.me/LINE_Developers_Agreement].
+//  This copyright notice shall be included in all copies or substantial portions of the software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import UIKit
+import LineSDK
+
+class SampleUIHomeViewController: UITableViewController {
+
+    enum Cell: Int {
+        case shareMessage
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == Cell.shareMessage.rawValue {
+            UIAlertController.present(in: self, error: "UI is not supported yet with Swift Package Manager integration")
+        }
+    }
+}
