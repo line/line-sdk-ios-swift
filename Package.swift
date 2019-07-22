f46c25ac50940191b0e4a437d47e13baf4e91211
onevcat <wei.wang@linecorp.com>
2019-07-22 11:51:26 +0900

    SPM support for ObjC wrapper target

--- Package.swift
+++ Package.swift
@@ -6,11 +6,18 @@ let package = Package(
     platforms: [.iOS(.v10)],
     products: [
         .library(name: "LineSDK", targets: ["LineSDK"]),
+        .library(name: "LineSDKObjC", targets: ["LineSDKObjC"])
     ],
     targets: [
         .target(
             name: "LineSDK",
             path: "LineSDK/LineSDK",
+            exclude: ["LineSDKUI"]
+        ),
+        .target(
+            name: "LineSDKObjC",
+            dependencies: ["LineSDK"],
+            path: "LineSDK/LineSDKObjC",
             exclude: ["LineSDKUI"]
         )
     ]
