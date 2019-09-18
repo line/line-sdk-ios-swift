f7e544536b5111942e50e4c7d11ecf4875c3f6dc
onevcat <wei.wang@linecorp.com>
2019-09-18 12:49:47 +0900

    Move color extensions to better place

--- LineSDK/LineSDK/Utils/Helpers.swift
+++ LineSDK/LineSDK/Utils/Helpers.swift
@@ -143,12 +143,3 @@ func guardSharedProperty<T>(_ input: T?) -> T {
     }
     return shared
 }
-
-extension UIColor {
-    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
-        }
-    }
-}
