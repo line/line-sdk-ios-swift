7c8d08868fa3c53b30bfafaf47cf6ffb32b508d6
onevcat <wei.wang@linecorp.com>
2019-12-12 16:49:50 +0900

    Remove LineSDKXCFramework flag

--- LineSDK/LineSDKObjC/Social/LineSDKUser.swift
+++ LineSDK/LineSDKObjC/Social/LineSDKUser.swift
@@ -19,7 +19,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if !LineSDKCocoaPods && !LineSDKXCFramework
+#if !LineSDKCocoaPods
 import LineSDK
 #endif
 
