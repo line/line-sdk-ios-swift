c1bf135392f1e44ee7b88e1fb5875c04d000d646
onevcat <wei.wang@linecorp.com>
2020-01-17 12:36:16 +0900

    Add strings for OpenChat category

--- LineSDK/LineSDK/OpenChat/Model/OpenChatCategory.swift
+++ LineSDK/LineSDK/OpenChat/Model/OpenChatCategory.swift
@@ -53,35 +53,35 @@ public enum OpenChatCategory: Int, CaseIterable {
 
 extension OpenChatCategory: CustomStringConvertible {
     public var description: String {
-        let text: String
+        let key: String
         switch self {
-        case .notSelected:  text = "Unselected (not shown in any category)"
-        case .school:       text = "Schools"
-        case .friend:       text = "Friends"
-        case .company:      text = "Company"
-        case .organization: text = "Organizations"
-        case .region:       text = "Local"
-        case .baby:         text = "Kids"
-        case .sports:       text = "Sports"
-        case .game:         text = "Games"
-        case .book:         text = "Books"
-        case .movies:       text = "Movies"
-        case .photo:        text = "Photos"
-        case .art:          text = "Art"
-        case .animation:    text = "Animation & comics"
-        case .music:        text = "Music"
-        case .tv:           text = "TV shows"
-        case .celebrity:    text = "Famous people"
-        case .food:         text = "Food"
-        case .travel:       text = "Travel"
-        case .pet:          text = "Pets"
-        case .car:          text = "Automotive"
-        case .fashion:      text = "Fashion & beauty"
-        case .health:       text = "Health"
-        case .finance:      text = "Finance & business"
-        case .study:        text = "Study"
-        case .etc:          text = "Other"
+        case .notSelected:  key = "square.create.category.notselected"
+        case .school:       key = "square.create.category.school"
+        case .friend:       key = "square.create.category.friend"
+        case .company:      key = "square.create.category.company"
+        case .organization: key = "square.create.category.org"
+        case .region:       key = "square.create.category.region"
+        case .baby:         key = "square.create.category.baby"
+        case .sports:       key = "square.create.category.sports"
+        case .game:         key = "square.create.category.game"
+        case .book:         key = "square.create.category.book"
+        case .movies:       key = "square.create.category.movies"
+        case .photo:        key = "square.create.category.photo"
+        case .art:          key = "square.create.category.art"
+        case .animation:    key = "square.create.category.ani"
+        case .music:        key = "square.create.category.music"
+        case .tv:           key = "square.create.category.tv"
+        case .celebrity:    key = "square.create.category.celebrity"
+        case .food:         key = "square.create.category.food"
+        case .travel:       key = "square.create.category.travel"
+        case .pet:          key = "square.create.category.pet"
+        case .car:          key = "square.create.category.car"
+        case .fashion:      key = "square.create.category.fashion"
+        case .health:       key = "square.create.category.health"
+        case .finance:      key = "square.create.category.finance"
+        case .study:        key = "square.create.category.study"
+        case .etc:          key = "square.create.category.etc"
         }
-        return text
+        return Localization.string(key)
     }
 }
