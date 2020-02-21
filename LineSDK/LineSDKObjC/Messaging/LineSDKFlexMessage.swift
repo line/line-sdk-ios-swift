c5dc9276f32cad79645e7c717e7d4913f4622430
onevcat <wei.wang@linecorp.com>
2020-02-21 16:36:42 +0900

    Add new scheme for building xcframework binary for objc wrapper
    It seems that Xcode does not support to build an xcframework depending on another xcframework right now. Some symbols are stripped and there is no way to make them right. So I created a binary to include all necessary files for ObjC SDK

--- LineSDK/LineSDKObjC/Messaging/LineSDKFlexMessage.swift
+++ LineSDK/LineSDKObjC/Messaging/LineSDKFlexMessage.swift
@@ -19,7 +19,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if !LineSDKCocoaPods
+#if !LineSDKCocoaPods && !LineSDKBinary
 import LineSDK
 #endif
 
--- LineSDK/LineSDKObjC/Utils/ResultExtensions.swift
+++ LineSDK/LineSDKObjC/Utils/ResultExtensions.swift
@@ -19,7 +19,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#if !LineSDKCocoaPods
+#if !LineSDKCocoaPods && !LineSDKBinary
 
 import LineSDK
 // These helper methods are not public since we do not want them to be exposed or cause any conflicts.
