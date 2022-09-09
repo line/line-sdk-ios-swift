a324305a8182b0134a9f4e8afb7dd424751dded2
JJ Lin <jj.lin@linecorp.com>
2022-09-09 17:04:52 +0800

    Remove hex3 hex4 in UIColor extension which are not used

--- LineSDK/LineSDK/Utils/Colors.swift
+++ LineSDK/LineSDK/Utils/Colors.swift
@@ -136,23 +136,6 @@ extension UIColor {
 
 extension UIColor {
 
-    convenience init(hex3: UInt16, alpha: CGFloat = 1) {
-        let divisor = CGFloat(15)
-        let red     = CGFloat((hex3 & 0xF00) >> 8) / divisor
-        let green   = CGFloat((hex3 & 0x0F0) >> 4) / divisor
-        let blue    = CGFloat( hex3 & 0x00F      ) / divisor
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-
-    convenience init(hex4: UInt16) {
-        let divisor = CGFloat(15)
-        let red     = CGFloat((hex4 & 0xF000) >> 12) / divisor
-        let green   = CGFloat((hex4 & 0x0F00) >>  8) / divisor
-        let blue    = CGFloat((hex4 & 0x00F0) >>  4) / divisor
-        let alpha   = CGFloat( hex4 & 0x000F       ) / divisor
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-
     convenience init(hex6: UInt32, alpha: CGFloat = 1) {
         let divisor = CGFloat(255)
         let red     = CGFloat((hex6 & 0xFF0000) >> 16) / divisor
@@ -184,8 +167,6 @@ extension UIColor {
             return
         }
         switch (hexString.count) {
-        case 3: self.init(hex3: UInt16(hexValue))
-        case 4: self.init(hex4: UInt16(hexValue))
         case 6: self.init(hex6: hexValue)
         case 8: self.init(hex8: hexValue)
         default: self.init(cgColor: color.cgColor)
