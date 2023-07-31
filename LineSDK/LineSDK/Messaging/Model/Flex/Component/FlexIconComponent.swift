1cc8f95d9a4d907827c543a04441a41a9984483f
onevcat <wei.wang@linecorp.com>
2023-07-31 14:22:47 +0900

    Rebrand LINE Corporation to LY Corporation

--- LineSDK/LineSDK/Messaging/Model/Flex/Component/FlexIconComponent.swift
+++ LineSDK/LineSDK/Messaging/Model/Flex/Component/FlexIconComponent.swift
@@ -1,13 +1,13 @@
 //
 //  FlexIconComponent.swift
 //
-//  Copyright (c) 2016-present, LINE Corporation. All rights reserved.
+//  Copyright (c) 2016-present, LY Corporation. All rights reserved.
 //
 //  You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
 //  copy and distribute this software in source code or binary form for use
-//  in connection with the web services and APIs provided by LINE Corporation.
+//  in connection with the web services and APIs provided by LY Corporation.
 //
-//  As with any software that integrates with the LINE Corporation platform, your use of this software
+//  As with any software that integrates with the LY Corporation platform, your use of this software
 //  is subject to the LINE Developers Agreement [http://terms2.line.me/LINE_Developers_Agreement].
 //  This copyright notice shall be included in all copies or substantial portions of the software.
 //
