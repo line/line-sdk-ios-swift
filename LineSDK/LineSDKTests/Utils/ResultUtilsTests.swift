f132ffcb6735f08e7567fe87e2a460490e9f30d0
onevcat <wei.wang@linecorp.com>
2024-10-23 13:17:34 +0900

    Reset file for Swift 4.2 compatibility

--- LineSDK/LineSDKTests/Utils/ResultUtilsTests.swift
+++ LineSDK/LineSDKTests/Utils/ResultUtilsTests.swift
@@ -45,7 +45,7 @@ class ResultUtilsTests: XCTestCase {
             _ = try failure.get()
             XCTFail("Cannot failure value.")
         } catch {
-            XCTAssertEqual(error, E.foo)
+            XCTAssertEqual(error as! E, E.foo)
         }
     }
 
